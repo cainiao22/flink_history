8b3e63efcb138f67d3e52ac417e54a7d8db84191
yanpengfei <yanpengfei@163.com>
2020-12-21 01:02:44 +0800

    完善function和aggregator

--- stratosphere-core/src/main/java/eu/stratosphere/api/common/distributions/SimpleDistribution.java
+++ stratosphere-core/src/main/java/eu/stratosphere/api/common/distributions/SimpleDistribution.java
@@ -17,28 +17,53 @@ public class SimpleDistribution implements DataDistribution {
 
     private static final long serialVersionUID = 1L;
 
-    protected Key[] boundaries;
+    protected Key[][] boundaries;
 
     protected int dim;
 
     public SimpleDistribution() {
-        this.boundaries = new Key[0];
+        this.boundaries = new Key[0][];
     }
 
     public SimpleDistribution(Key[] boundaries) {
         this.dim = 1;
-        if(boundaries == null || boundaries.length == 0){
+        if (boundaries == null || boundaries.length == 0) {
             throw new IllegalArgumentException("The bucket boundaries are of different class types.");
         }
-        this.boundaries = new Key[boundaries.length];
+        this.boundaries = new Key[boundaries.length][];
         Class<? extends Key> clazz = boundaries[0].getClass();
         for (int i = 0; i < boundaries.length; i++) {
-            if(boundaries[i].getClass() != clazz){
+            if (boundaries[i].getClass() != clazz) {
                 throw new IllegalArgumentException("The bucket boundaries are of different class types.");
             }
-            this.boundaries[i] = boundaries[i];
+            this.boundaries[i] = new Key[]{boundaries[i]};
         }
     }
+
+
+    public SimpleDistribution(Key[][] boundaries) {
+        if (boundaries == null || boundaries.length == 0) {
+            throw new IllegalArgumentException("The bucket boundaries are of different class types.");
+        }
+        this.dim = boundaries[0].length;
+        Class<? extends Key>[] types = new Class[dim];
+        for (int i = 0; i < this.boundaries.length; i++) {
+            types[i] = boundaries[0][i].getClass();
+        }
+        for (int i = 0; i < boundaries.length; i++) {
+            if (boundaries[i].length != dim) {
+                throw new IllegalArgumentException("All bucket boundaries must have the same dimensionality.");
+            }
+            for (int j = 0; j < boundaries[i].length; j++) {
+                if (types[j] != boundaries[i][j].getClass()) {
+                    throw new IllegalArgumentException("The bucket boundaries are of different class types.");
+                }
+            }
+        }
+
+        this.boundaries = boundaries;
+    }
+
 
     @Override
     public Key[] getBucketBoundary(int bucketNum, int totalNumBuckets) {
@@ -47,7 +72,7 @@ public class SimpleDistribution implements DataDistribution {
 
     @Override
     public int getNumberOfFields() {
-        return 0;
+        return this.dim;
     }
 
     @Override
