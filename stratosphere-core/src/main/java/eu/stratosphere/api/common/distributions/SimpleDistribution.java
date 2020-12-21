a16924bde31208a8c88c8d5ec15b1a7b39e1aa92
yanpengfei <yanpengfei@baijiahulian.com>
2020-12-21 21:10:15 +0800

    SimpleDistribution 完善

--- stratosphere-core/src/main/java/eu/stratosphere/api/common/distributions/SimpleDistribution.java
+++ stratosphere-core/src/main/java/eu/stratosphere/api/common/distributions/SimpleDistribution.java
@@ -7,7 +7,7 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 /**
- * TODO
+ * 简易分布式数据
  *
  * @author Administrator
  * @version 1.0
@@ -28,13 +28,15 @@ public class SimpleDistribution implements DataDistribution {
     public SimpleDistribution(Key[] boundaries) {
         this.dim = 1;
         if (boundaries == null || boundaries.length == 0) {
-            throw new IllegalArgumentException("The bucket boundaries are of different class types.");
+            throw new IllegalArgumentException(
+                "The bucket boundaries are of different class types.");
         }
         this.boundaries = new Key[boundaries.length][];
         Class<? extends Key> clazz = boundaries[0].getClass();
         for (int i = 0; i < boundaries.length; i++) {
             if (boundaries[i].getClass() != clazz) {
-                throw new IllegalArgumentException("The bucket boundaries are of different class types.");
+                throw new IllegalArgumentException(
+                    "The bucket boundaries are of different class types.");
             }
             this.boundaries[i] = new Key[]{boundaries[i]};
         }
@@ -43,7 +45,8 @@ public class SimpleDistribution implements DataDistribution {
 
     public SimpleDistribution(Key[][] boundaries) {
         if (boundaries == null || boundaries.length == 0) {
-            throw new IllegalArgumentException("The bucket boundaries are of different class types.");
+            throw new IllegalArgumentException(
+                "The bucket boundaries are of different class types.");
         }
         this.dim = boundaries[0].length;
         Class<? extends Key>[] types = new Class[dim];
@@ -52,11 +55,13 @@ public class SimpleDistribution implements DataDistribution {
         }
         for (int i = 0; i < boundaries.length; i++) {
             if (boundaries[i].length != dim) {
-                throw new IllegalArgumentException("All bucket boundaries must have the same dimensionality.");
+                throw new IllegalArgumentException(
+                    "All bucket boundaries must have the same dimensionality.");
             }
             for (int j = 0; j < boundaries[i].length; j++) {
                 if (types[j] != boundaries[i][j].getClass()) {
-                    throw new IllegalArgumentException("The bucket boundaries are of different class types.");
+                    throw new IllegalArgumentException(
+                        "The bucket boundaries are of different class types.");
                 }
             }
         }
@@ -65,9 +70,38 @@ public class SimpleDistribution implements DataDistribution {
     }
 
 
+    /**
+     * 获取某个桶的边界
+     *
+     * @param bucketNum       index
+     * @param totalNumBuckets 总数
+     * @return
+     */
     @Override
     public Key[] getBucketBoundary(int bucketNum, int totalNumBuckets) {
-        return new Key[0];
+        if (bucketNum < 0) {
+            throw new IllegalArgumentException(
+                "Requested bucket must be greater than or equal to 0.");
+        } else if (bucketNum >= (totalNumBuckets - 1)) {
+            throw new IllegalArgumentException(
+                "Request bucket must be smaller than the total number of buckets minus 1.");
+        }
+        if (totalNumBuckets < 1) {
+            throw new IllegalArgumentException("Total number of bucket must be larger than 0.");
+        }
+
+        int maxNumBuckets = this.boundaries.length + 1;
+        if (maxNumBuckets % totalNumBuckets == 0) {
+            int n = maxNumBuckets / totalNumBuckets;
+            int bucketIndex = bucketNum * n + n - 1;
+            return boundaries[bucketIndex];
+        } else {
+            throw new IllegalArgumentException(
+                "Interpolation of bucket boundaries currently not supported. " +
+                    "Please use an even divider of the maximum possible buckets (here: "
+                    + maxNumBuckets + ") as totalBuckets.");
+            // TODO: might be relaxed if much more boundary records are available than requested
+        }
     }
 
     @Override
@@ -77,11 +111,22 @@ public class SimpleDistribution implements DataDistribution {
 
     @Override
     public void read(DataInput input) throws IOException {
-
+        this.dim = input.readInt();
+        this.boundaries = new Key[input.readInt()][dim];
+        //for()
     }
 
     @Override
     public void write(DataOutput out) throws IOException {
-
+        out.writeInt(dim);
+        out.writeInt(this.boundaries.length);
+        for (int i = 0; i < dim; i++) {
+            out.writeUTF(this.boundaries[0][i].getClass().getName());
+        }
+        for (int i = 0; i < this.boundaries.length; i++) {
+            for (int j = 0; j < dim; j++) {
+                boundaries[i][j].write(out);
+            }
+        }
     }
 }
